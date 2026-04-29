9d32a4479a6146d19007b64e70d096defd74cacf
flavvvviak10-creator <flavvvviak10@gmail.com>
2026-04-29 14:16:40 -0300

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,18 +1,27 @@
 <?php 
 
-/*=====================================
-▀▀█▀▀ █  █ █▀▀▀    █▀▀▀ █▀▀█ █ ▄▀ █▀▀▀ 
-  █   █▀▀█ █▀▀▀ ▀▀ █▀▀▀ █▄▄█ █▀▄  █▀▀▀ 
-  █   █  █ █▄▄▄    █    █  █ █  █ █▄▄▄
-=====================================*/
-
 class db {
-public static $db_server = "localhost";
-public static $db_db = "switchback.proxy.rlwy.net";
-public static $db_user = "root";
-public static $db_pass = "eVEFVwHlbgXyYJdYjKYjpveWscIsilIC";
+    public static $db_server = "";
+    public static $db_db = "";
+    public static $db_user = "";
+    public static $db_pass = "";
+    public static $db_port = "";
+
+    public static function init() {
+        self::$db_server = getenv("MYSQLHOST") ?: "localhost";
+        self::$db_db = getenv("MYSQLDATABASE") ?: "railway";
+        self::$db_user = getenv("MYSQLUSER") ?: "root";
+        self::$db_pass = getenv("MYSQLPASSWORD") ?: "eVEFVwHlbgXyYJdYjKYjpveWscIsilIC";
+        self::$db_port = getenv("MYSQLPORT") ?: "29780";
+    }
 }
-$conn = mysqli_connect(db::$db_server, db::$db_user, db::$db_pass, db::$db_db);
-if (!$conn) { die("Conexao falhou: " . mysqli_connect_error()); }
+
+db::init();
+
+$conn = mysqli_connect(db::$db_server, db::$db_user, db::$db_pass, db::$db_db, db::$db_port);
+
+if (!$conn) { 
+    die("Conexao falhou: " . mysqli_connect_error()); 
+}
 
 ?>
