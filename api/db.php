34c88a41197abe96933b91f2081b2d6e0be1c6cb
flavvvviak10-creator <flavvvviak10@gmail.com>
2026-04-29 13:29:37 -0300

    Update db.php

--- api/db.php
+++ api/db.php
@@ -7,10 +7,10 @@
 =====================================*/
 
 class db {
-public static $db_server = "switchback.proxy.rlwy.net";
-public static $db_db = "railway";
+public static $db_server = "localhost";
+public static $db_db = "switchback.proxy.rlwy.net";
 public static $db_user = "root";
-public static $db_pass = "RhTNMVFzagwXunmRcCgbOAZdlymxLNCY";
+public static $db_pass = "eVEFVwHlbgXyYJdYjKYjpveWscIsilIC";
 }
 $conn = mysqli_connect(db::$db_server, db::$db_user, db::$db_pass, db::$db_db);
 if (!$conn) { die("Conexao falhou: " . mysqli_connect_error()); }
